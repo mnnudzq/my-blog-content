4450d5752ef2c28a42472bf40f1c85f9fbef1328
dengzhiqiang <zhiqiang.deng@fft.com.cn>
2019-12-19 13:55:23 +0800

    【array常用方法】

---
+++ arrayAndDataDirveView/data.js
@@ -0,0 +1,26 @@
+let data = [{
+    id: '44a2ab382b5e41c288b112a745344c9c',
+    name: '这个杀手不太冷',
+    score: 9.4,
+    collect: false
+}, {
+    id: '522e5d4ea39149539bcc9a1af6733487',
+    name: '蝙蝠侠：黑暗骑士',
+    score: 9.2,
+    collect: false
+}, {
+    id: 'a07561701a9d42dfb8c65fd2bce425aa',
+    name: '七武士',
+    score: 9.2,
+    collect: false
+}, {
+    id: '44a2ab382b5e41c288b112a745344542',
+    name: '将军号',
+    score: 9.1,
+    collect: false
+}, {
+    id: '44a2ab382b5e41c288b112a745344639',
+    name: '让子弹飞',
+    score: 8.8,
+    collect: false
+}];
